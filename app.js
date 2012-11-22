22319df65e6f066d26e34f5994731c1e83da8743
Richard Rodger <richard@ricebridge.com>
2012-11-22 00:27:59 +0000

    config port

--- app.js
+++ app.js
@@ -8,21 +8,26 @@ var si = seneca({log:'print'})
 si.use( 'config', {object:require('./config.mine.js')} )
 si.use( require('./lib/nodezoo') )
 
+si.act({role:'config',cmd:'get',base:'nodezoo'}, function(err,config){
+  if( err ) throw err;
+  start_express(config)
+})
 
 
-var app = express()
+function start_express(config) {
+  var app = express()
 
-app.use(express.cookieParser())
-app.use(express.query())
-app.use(express.bodyParser())
-app.use(express.methodOverride())
-app.use(express.json())
-app.use(express.static(__dirname+'/site/public'))
-//app.use(express.session({ secret: 'waterford' }))
+  app.use(express.cookieParser())
+  app.use(express.query())
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(express.json())
+  app.use(express.static(__dirname+'/site/public'))
+  //app.use(express.session({ secret: 'waterford' }))
 
 
-app.use( si.service() )
+  app.use( si.service() )
 
-app.listen(8101)
-
+  app.listen( config.web.port )
+}
 
