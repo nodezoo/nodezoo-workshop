f5a451533e6ccbed177cb1df4e5a52ce35c7ca1d
Richard Rodger <richard@ricebridge.com>
2012-11-24 00:35:39 +0000

    update deps

--- bin/update.js
+++ bin/update.js
@@ -1,9 +1,21 @@
 
 var fs       = require('fs')
-var request  = require('request');
-var optimist = require('optimist');
-var nodezoo  = require('..')
+var path     = require('path')
 
+var request  = require('request')
+var optimist = require('optimist')
+var byline   = require('byline')
+var js       = require('JSONStream')
+var _        = require('underscore')
+
+var nodezoo  = require('../lib/nodezoo.js')
+
+
+/*
+node update.js -d ids 
+node update.js -d all -f ../data/npm.json
+node update.js -p -f ../data/npm.json
+*/
 
 
 function die(err) {
@@ -34,15 +46,56 @@ function download( filepath, url, cb ) {
     .pipe(fs.createWriteStream(filepath)  
           .on('error',die)
           .on('close',function(){
-            process.stdout.write('\n')
-            console.log('total:'+total)
+            console.log('\nbyte-size:'+total)
             cb()
           }))
 }
 
 
-function rank(){
-  console.log('rank')
+function makels(filepath,online,onend) {
+  var rs=fs.createReadStream(filepath)
+  rs.setEncoding('ascii')
+  var ls = byline.createStream()
+  online && ls.on('data', online) 
+  onend  && ls.on('end',  onend) 
+  rs.pipe(ls)
+  return ls
+}
+
+
+
+function deps(filepath) {
+  console.log('Dependencies...')
+
+  var fw = fs.WriteStream( path.dirname(filepath) +'/deps.json' )
+  var jsw = js.stringify()
+  jsw.pipe(fw)
+
+  var index = 0
+
+  var ls = makels(
+    filepath, 
+    function(line){
+      try {
+        var data = JSON.parse(line.substring(0,line.length-1)).value
+      
+        var latest = (data['dist-tags']||{}).latest
+        var deps   = (data.versions && latest) ? _.keys( (data.versions[latest]||{}).dependencies || {} ) : []
+        jsw.write({i:index,m:data.name,d:deps})
+        index++
+
+        if( 0 == index % 100 ) {
+          process.stdout.write('.')
+        }
+      }
+      catch(e) {
+        console.log(e)
+      }
+    },
+    function(){
+      console.log('\nmodule-count:'+index)
+      jsw.end()
+    })
 }
 
 
@@ -55,11 +108,12 @@ if( argv.d ) {
   var url = urls[argv.d]
   console.log('Downloading '+url+' to '+filepath)
   download(filepath,url,function(){
-    if( argv.r ) {
-      rank()
+    if( argv.p ) {
+      deps(filepath)
     }
   })
 }
-else if( argv.r ) {
-  rank()
+else if( argv.p ) {
+  filepath = argv.f
+  deps(filepath)
 }
