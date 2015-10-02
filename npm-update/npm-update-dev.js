6b6ea2740714b30e3f0c1ecc26b4f9e5e75e5194
Richard Rodger <richard@ricebridge.com>
2015-10-02 19:09:47 +0100

    web

--- npm-update/npm-update-dev.js
+++ npm-update/npm-update-dev.js
@@ -1,6 +1,7 @@
 require('seneca')()
   .use('level-store')
   .use('beanstalk-transport')
+  .use('../../seneca-msgstats',{pin:'role:npm,info:change'})
   .use('../../nodezoo-npm-update')
 
   .client({pin:'role:npm,info:change',type:'beanstalk'})
--- search/search-dev.js
+++ search/search-dev.js
@@ -1,5 +1,8 @@
 require('seneca')()
+  .use('../../seneca-msgstats',{pin:'role:search,cmd:search'})
   .use('../../nodezoo-index')
 
   .listen(9003)
   .repl(44003)
+
+
