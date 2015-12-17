ce4377cbf7942b9460be0bac28aba8c5079eaa73
Richard Rodger <richard@ricebridge.com>
2015-12-17 12:00:32 +0000

    ratios

--- system/msgstats.js
+++ system/msgstats.js
@@ -2,4 +2,8 @@
 var HOST = process.env.HOST || 'localhost'
 
 require('seneca')()
-  .use('msgstats',{udp:{host:HOST},collect:true})
+  .use('msgstats',{
+    udp:{host:HOST},
+    collect:true,
+    ratios:[['res:part,role:info','req:part,role:info']]
+  })
