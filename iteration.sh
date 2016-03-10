a3516e52efcf7a3c4a4ee5afed21c84f22135f9e
Jane Quinn <jane.quinn@nearform.com>
2016-03-10 15:08:15 +0000

    added iteration script

---
+++ iteration.sh
@@ -0,0 +1,28 @@
+ITER=${1:-i00}
+echo "Moving to iteration:" $ITER
+
+echo "nodezoo-web " $ITER
+cd ../nodezoo-web
+git checkout $ITER
+
+echo "nodezoo-search " $ITER
+cd ../nodezoo-search
+git checkout $ITER
+
+echo "nodezoo-info " $ITER
+cd ../nodezoo-info
+git checkout $ITER
+
+echo "nodezoo-npm " $ITER
+cd ../nodezoo-npm
+git checkout $ITER
+
+echo "nodezoo-github " $ITER
+cd ../nodezoo-github
+git checkout $ITER
+
+echo "nodezoo-npm-update " $ITER
+cd ../nodezoo-npm-update
+git checkout $ITER
+
+cd ../nodezoo
